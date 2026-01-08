fb49f103c470742d9cb654e9dc12e25cceb25652
Rose22 <rosemelody22@gmail.com>
2026-01-08 07:36:14 +0100

    made it stop exiting on python exceptions

--- aish.py
+++ aish.py
@@ -50,12 +50,16 @@ def confirm(prompt):
 def toggle_bool(thebool, description="feature"):
     if thebool:
         thebool = False
+        print_color(description, colored.Fore.sky_blue_1)
         print(f"{colored.Fore.sky_blue_1}{description}{colored.Style.reset} turned off")
     else:
         thebool = True
         print(f"{colored.Fore.sky_blue_1}{description}{colored.Style.reset} turned on")
 
     return thebool
+
+def print_color(message, color, **kwargs):
+    print(f"{color}{message}{colored.Style.reset}", *kwargs)
 
 def process_cmd(cmd):
     cmd_split = cmd.split(" ")
@@ -65,10 +69,7 @@ def process_cmd(cmd):
         case "cd":
             # actually change directory
             target_path = os.path.expanduser(" ".join(cmd_split[1:]))
-            try:
-                os.chdir(target_path)
-            except Exception as e:
-                print(f"failed to change directory: {e}")
+            os.chdir(target_path)
             return False
 
     # substitute certain commands for different ones
@@ -159,149 +160,153 @@ except Exception as e:
     print("normal shell mode engaged")
 
 while True:
-    print()
+    try:
+        print()
 
-    path_display = os.getcwd().replace(os.path.expanduser("~"), "~")
-    shell_prompt = f"{colored.Fore.green}AI" if using_ai else f"{colored.Fore.sky_blue_1}shell"
-    shell_prompt += colored.Style.reset
-    shell_prompt += f" ({path_display})"
+        path_display = os.getcwd().replace(os.path.expanduser("~"), "~")
+        shell_prompt = f"{colored.Fore.green}AI" if using_ai else f"{colored.Fore.sky_blue_1}shell"
+        shell_prompt += colored.Style.reset
+        shell_prompt += f" ({path_display})"
 
-    cmd = input(f"{shell_prompt}> ")
-    cmd_split = cmd.split(" ")
+        cmd = input(f"{shell_prompt}> ")
+        cmd_split = cmd.split(" ")
 
-    match cmd:
-        case "exit":
-            exit()
-            break
-        case "auto":
-            auto = toggle_bool(auto, "automatic command execution")
-            if not auto:
-                hide_cmd = False
-        case "hide":
-            if not auto:
-                print("turn on automatic execution first with 'auto'")
-                continue
-
-            hide_cmd = toggle_bool(hide_cmd, "command hiding")
-        case "connect":
-            if using_ai:
-                print("already connected!")
-                continue
-
-            try:
-                client.chat.completions.create(
-                    model="model",
-                    messages=check_prompt
-                )
-                using_ai = True
-            except Exception as e:
-                print(f"failed to connect to AI! error: {e}")
-                continue
-        case "disconnect":
-            if not using_ai:
-                print("already disconnected!")
-
-            using_ai = False
-        case "help":
-            print("""
-exit:       exit the shell
-auto:       toggle auto execution mode (WARNING: dangerous! disables confirmation before running suggested commands. will still ask for confirmation when running root commands)
-hide:       toggle command hiding (hides generated commands prior to running them)
-connect:    reconnect to the AI in case a disconnection occured
-disconnect: disconnect from the AI, switch to an AI-less shell
-help:       display help
-
-Type what you want the shell to do, then press enter. The AI will then generate a shell command and ask you if you want to run it.
-You can also just type normal shell commands, which will run if the AI doesn't modify the command.
-
-You can find and target files within the current folder (even nested folders) by prepending the filename with a '@'. Example: cat @aish.py will search for the file and then read it.
-""".strip())
-        case "":
-            pass
-        case _:
-            if not using_ai:
-                # just execute the command like a normal shell
-                os.system(cmd)
-                continue
-
-            # recursively retrieve the file structure from the current directory and use it to find and target any paths the user has specified with a @
-            dir_tree = False
-            relevant_paths = []
-            for word in cmd_split:
-                if word[0] == "@":
-                    print(f"{colored.Fore.sky_blue_1}>> targeting {word[1:]}{colored.Style.reset}")
-                    if not dir_tree:
-                        dir_tree = recursive_list(".", max_depth=3)
-
-                    for item in dir_tree:
-                        if word[1:] in item:
-                            relevant_paths.append(item)
-
-            if relevant_paths:
-                relevant_paths = f"\nYou can find target files at one of these paths: {relevant_paths}"
-
-            prompt = [
-                {
-                    "role": "system",
-                    "content": f"You are currently in directory `{os.getcwd()}`.\nUser's home directory is `{os.path.expanduser('~')}`.\n{relevant_paths}\nThe current date is {datetime.datetime.now().strftime('%b %d %Y %H:%M:%S')}.\nFiles in current directory: {os.listdir()}.\nSystem information: {sys_info}"
-                },
-                {
-                    "role": "system",
-                    "content": ai_prompt
-                },
-                {
-                    "role": "user",
-                    "content": cmd
-                }
-            ]
-
-            try:
-                stream = client.chat.completions.create(
-                    model="model",
-                    messages=prompt,
-                    stream=True
-                )
-
-                # stream llm's response
-                chunks = []
-                if not hide_cmd:
-                    print(f"{colored.Fore.sky_blue_1}>> ", end="")
-                for chunk in stream:
-                    chunk_s = chunk.choices[0].delta.content
-                    chunks.append(chunk_s)
-
-                    if not hide_cmd:
-                        print(chunk_s, end="", flush=True)
-                if not hide_cmd:
-                    print(colored.Style.reset)
-
-                ai_cmd = "".join(chunks)
-                ai_cmd_split = ai_cmd.split(" ")
-            except Exception as e:
-                print(f"failed to connect to AI! error: {e}")
-                using_ai = False
-                continue
-
-            env_vars = os.environ.copy()
-
-            if ai_cmd.lower().strip() == cmd.lower().strip():
-                # just run it if it's the same as what the user typed - it's probably a shell command the user entered
-
-                ai_cmd = process_cmd(ai_cmd)
-                if ai_cmd:
-                    subprocess.run(ai_cmd, env=env_vars, shell=True, text=True)
-            else:
-                if ai_cmd_split[0].lower() in ("sudo", "su"):
-                    if hide_cmd:
-                        print(f">> {ai_cmd}")
-
-                    if not confirm(f"{colored.Fore.red}really execute as root{colored.Style.reset}"):
-                        continue
-
+        match cmd:
+            case "exit":
+                exit()
+                break
+            case "auto":
+                auto = toggle_bool(auto, "automatic command execution")
                 if not auto:
-                    if not confirm(f"{colored.Fore.green}execute{colored.Style.reset}"):
-                        continue
+                    hide_cmd = False
+            case "hide":
+                if not auto:
+                    print("turn on automatic execution first with 'auto'")
+                    continue
 
-                ai_cmd = process_cmd(ai_cmd)
-                if ai_cmd:
-                    subprocess.run(ai_cmd, env=env_vars, shell=True, text=True)
+                hide_cmd = toggle_bool(hide_cmd, "command hiding")
+            case "connect":
+                if using_ai:
+                    print("already connected!")
+                    continue
+
+                try:
+                    client.chat.completions.create(
+                        model="model",
+                        messages=check_prompt
+                    )
+                    using_ai = True
+                except Exception as e:
+                    print(f"failed to connect to AI! error: {e}")
+                    continue
+            case "disconnect":
+                if not using_ai:
+                    print("already disconnected!")
+
+                using_ai = False
+            case "help":
+                print("""
+    exit:       exit the shell
+    auto:       toggle auto execution mode (WARNING: dangerous! disables confirmation before running suggested commands. will still ask for confirmation when running root commands)
+    hide:       toggle command hiding (hides generated commands prior to running them)
+    connect:    reconnect to the AI in case a disconnection occured
+    disconnect: disconnect from the AI, switch to an AI-less shell
+    help:       display help
+
+    Type what you want the shell to do, then press enter. The AI will then generate a shell command and ask you if you want to run it.
+    You can also just type normal shell commands, which will run if the AI doesn't modify the command.
+
+    You can find and target files within the current folder (even nested folders) by prepending the filename with a '@'. Example: cat @aish.py will search for the file and then read it.
+    """.strip())
+            case "":
+                pass
+            case _:
+                if not using_ai:
+                    # just execute the command like a normal shell
+                    os.system(cmd)
+                    continue
+
+                # recursively retrieve the file structure from the current directory and use it to find and target any paths the user has specified with a @
+                dir_tree = False
+                relevant_paths = []
+                for word in cmd_split:
+                    if word[0] == "@":
+                        print(f"{colored.Fore.sky_blue_1}>> targeting {word[1:]}{colored.Style.reset}")
+                        if not dir_tree:
+                            dir_tree = recursive_list(".", max_depth=3)
+
+                        for item in dir_tree:
+                            if word[1:] in item:
+                                relevant_paths.append(item)
+
+                if relevant_paths:
+                    relevant_paths = f"\nYou can find target files at one of these paths: {relevant_paths}"
+
+                prompt = [
+                    {
+                        "role": "system",
+                        "content": f"You are currently in directory `{os.getcwd()}`.\nUser's home directory is `{os.path.expanduser('~')}`.\n{relevant_paths}\nThe current date is {datetime.datetime.now().strftime('%b %d %Y %H:%M:%S')}.\nFiles in current directory: {os.listdir()}.\nSystem information: {sys_info}"
+                    },
+                    {
+                        "role": "system",
+                        "content": ai_prompt
+                    },
+                    {
+                        "role": "user",
+                        "content": cmd
+                    }
+                ]
+
+                try:
+                    stream = client.chat.completions.create(
+                        model="model",
+                        messages=prompt,
+                        stream=True
+                    )
+
+                    # stream llm's response
+                    chunks = []
+                    if not hide_cmd:
+                        print(f"{colored.Fore.sky_blue_1}>> ", end="")
+                    for chunk in stream:
+                        chunk_s = chunk.choices[0].delta.content
+                        chunks.append(chunk_s)
+
+                        if not hide_cmd:
+                            print(chunk_s, end="", flush=True)
+                    if not hide_cmd:
+                        print(colored.Style.reset)
+
+                    ai_cmd = "".join(chunks)
+                    ai_cmd_split = ai_cmd.split(" ")
+                except Exception as e:
+                    print(f"failed to connect to AI! error: {e}")
+                    using_ai = False
+                    continue
+
+                env_vars = os.environ.copy()
+
+                if ai_cmd.lower().strip() == cmd.lower().strip():
+                    # just run it if it's the same as what the user typed - it's probably a shell command the user entered
+
+                    ai_cmd = process_cmd(ai_cmd)
+                    if ai_cmd:
+                        subprocess.run(ai_cmd, env=env_vars, shell=True, text=True)
+                else:
+                    if ai_cmd_split[0].lower() in ("sudo", "su"):
+                        if hide_cmd:
+                            print(f">> {ai_cmd}")
+
+                        if not confirm(f"{colored.Fore.red}really execute as root{colored.Style.reset}"):
+                            continue
+
+                    if not auto:
+                        if not confirm(f"{colored.Fore.green}execute{colored.Style.reset}"):
+                            continue
+
+                    ai_cmd = process_cmd(ai_cmd)
+                    if ai_cmd:
+                        subprocess.run(ai_cmd, env=env_vars, shell=True, text=True)
+    except Exception as e:
+        print_color(f"error: {e}", colored.Fore.red)
+        pass
